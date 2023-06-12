5ba4280a168d827065e5dade556e4fd55ed42acb
Stephanie Park <js35park@uwaterloo.ca>
2023-06-11 22:38:05 -0400

    set up navigation menu

--- FarmBuddy/app/src/main/java/com/example/farmbuddy/MainActivity.kt
+++ FarmBuddy/app/src/main/java/com/example/farmbuddy/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home_mode, R.id.navigation_farm_mode, R.id.navigation_charity_mode, R.id.navigation_market_mode
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
