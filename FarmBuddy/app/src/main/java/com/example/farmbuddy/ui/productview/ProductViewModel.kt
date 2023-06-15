46b32852f6fcb30d4ff821a65b884fbd74ab2dca
paulchang <changpaul333@gmail.com>
2023-06-15 14:38:01 -0400

    adding productview component

---
+++ FarmBuddy/app/src/main/java/com/example/farmbuddy/ui/productview/ProductViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.farmbuddy.ui.productview
+
+import androidx.lifecycle.ViewModel
+
+class ProductViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ FarmBuddy/app/src/main/java/com/example/farmbuddy/ui/productview/ProductViewReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.farmbuddy.ui.productview
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ProductViewReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("ProductViewReceiver.onReceive() is not implemented")
+    }
+}
