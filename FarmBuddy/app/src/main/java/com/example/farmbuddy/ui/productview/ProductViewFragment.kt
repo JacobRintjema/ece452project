cce6451e3ca094e2c1def13d4578cdc942f77ba6
paulchang <changpaul333@gmail.com>
2023-06-23 21:55:21 -0400

    temp commit

--- FarmBuddy/app/src/main/java/com/example/farmbuddy/ui/productview/ProductViewFragment.kt
+++ FarmBuddy/app/src/main/java/com/example/farmbuddy/ui/productview/ProductViewFragment.kt
@@ -1,14 +1,26 @@
 package com.example.farmbuddy.ui.productview
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.core.view.size
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.farmbuddy.R
+import com.example.farmbuddy.databinding.FragmentProductViewBinding
+
 
 class ProductViewFragment : Fragment() {
+
+    private var _binding: FragmentProductViewBinding? = null
+
+    private val binding get() = _binding!!
+
 
     companion object {
         fun newInstance() = ProductViewFragment()
@@ -20,12 +32,45 @@ class ProductViewFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_product_view, container, false)
+        val productViewModel =
+            ViewModelProvider(this).get(ProductViewModel::class.java)
+
+        _binding = FragmentProductViewBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textMarketMode
+        productViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+
+
+        return root
+
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ProductViewModel::class.java)
+        val table = binding.marketTable
+        val button = binding.imgAdd
+        button.setOnClickListener{
+            val tr = TableRow(activity)
+            val tv1 = TextView(activity)
+            tv1.text = "TEST NUMBER"
+            tv1.textSize = 20f
+            tv1.setPadding(5, 5, 5, 5)
+            tr.addView(tv1)
+            table.addView(tr)
+        }
+
+        val button2 = binding.imgRemove
+        button2.setOnClickListener{
+            if (table.size > 0) {
+                table.removeViewAt(table.size-1)
+            }
+
+        }
         // TODO: Use the ViewModel
     }
 
